d144212edffd74131a6660bad4137ffac647f9f1
yeyuxuan <yeyuxuan@datarc.cn>
2023-07-16 23:34:43 +0800

    63 Unique Paths II

---
+++ 63. 不同路径 II (Unique Paths II)/Template.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid = []) {
+
+};
+
+console.log(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])) // 2
+console.log(uniquePathsWithObstacles([[0,1],[0,0]])) // 1
